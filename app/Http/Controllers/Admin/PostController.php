01a88979a946566b9da20511eb959fd71c759e8a
sergey-emelyanov <emelia35@yandex.ru>
2026-04-18 05:13:44 -0400

    Добавлена анотация методов в PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -9,6 +9,13 @@ use App\Http\Resources\Post\PostResource;
 
 class PostController extends Controller
 {
+    /**
+     *  Метод отвечающий за показ всех постов
+     *
+     *  [GET] /admin/posts
+     *
+     *  @return inertia/response
+     */
     public function index()
     {
         $posts = Post::all();
@@ -17,17 +24,42 @@ class PostController extends Controller
         return inertia('Admin/Post/Index', compact('posts'));
     }
 
+    /**
+     *  Метод отвечающий за показ поста
+     *
+     *  [GET] /admin/posts/{post}/
+     *
+     *  @param Post $post
+     *  @return inertia/response
+     */
     public function show(Post $post)
     {
         $post = PostResource::make($post)->resolve();
         return inertia('Admin/Post/Show', compact('post'));
     }
 
+
+    /**
+     *  Отображает форму создания поста
+     *
+     *  [GET] /admin/posts/create/
+     *
+     *  @return inertia/response
+     */
     public function create()
     {
         return inertia('Admin/Post/Create');
     }
 
+
+    /**
+     * Метод отвечающий за создания поста и запись в бд
+     *
+     *  [POST] /admin/posts/post/
+     *
+     * @param StoreRequest $request
+     * @return PostResource
+     */
     public function store(StoreRequest $request)
     {
         $data = $request->validated();
